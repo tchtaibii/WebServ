eeeecdfa1095cbf04951263d97f3a108347e27e8
Taha Chtaibi <tchtaibi@e2r4p9.1337.ma>
2023-03-04 17:17:30 +0100

    method with select

--- Socket/socket.hpp
+++ Socket/socket.hpp
@@ -6,7 +6,7 @@
 /*   By: tchtaibi <tchtaibi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 15:02:16 by tchtaibi          #+#    #+#             */
-/*   Updated: 2023/03/03 16:01:42 by tchtaibi         ###   ########.fr       */
+/*   Updated: 2023/03/04 17:17:08 by tchtaibi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,103 +21,171 @@
 #include "parsingRequest.hpp"
 #include "../confParsing/parsingConf.hpp"
 
-namespace ws{
+namespace ws
+{
     class Socket
     {
-        private:
-            int     sock;
-            int     connection;
-            int     listening;
-            int     backlog;
-            int     new_socket;
-            int     domain;
-            u_long  interface;
-            int     port;
-            int     service;
-            int     protocol;
-            struct sockaddr_in address;
-        public:
-            Socket(int back_log = 10000, int port = 8000, u_long interface = INADDR_ANY, int domain = AF_INET, int service = SOCK_STREAM, int protocol = 0)
+    private:
+        int sock;
+        int connection;
+        int listening;
+        int backlog;
+        int new_socket;
+        int domain;
+        u_long interface;
+        int port;
+        int service;
+        int protocol;
+        struct sockaddr_in address;
+
+    public:
+        Socket(int back_log = 10000, int port = 8000, u_long interface = INADDR_ANY, int domain = AF_INET, int service = SOCK_STREAM, int protocol = 0)
+        {
+            this->backlog = back_log;
+            this->port = port;
+            this->interface = interface;
+            this->domain = domain;
+            this->service = service;
+            this->protocol = protocol;
+        }
+        void start(server servers)
+        {
+            // Etablish socket
+            sock = socket(domain, service, protocol);
+            if (sock == 0)
             {
-                this->backlog = back_log;
-                this->port = port;
-                this->interface = interface;
-                this->domain = domain;
-                this->service = service;
-                this->protocol = protocol;
+                perror("In socket...");
+                exit(EXIT_FAILURE);
             }
-            void start(server servers)
+            // init adress struct
+            address.sin_family = domain;
+            address.sin_addr.s_addr = htonl(interface);
+            address.sin_port = htons(port);
+            memset(address.sin_zero, '\0', sizeof address.sin_zero);
+            // binding the connection
+            connection = bind(sock, (struct sockaddr *)&address, sizeof(address));
+            test_connection(connection);
+            // listen
+            listening = listen(sock, backlog);
+            test_connection(listening);
+            // ft_accept(servers);
+        }
+        std::map<std::string, location>::iterator locationChecker(std::string path, std::map<std::string, location> &Location)
+        {
+            std::vector<std::string> pathComponents; // vector of paths
+            std::stringstream ss(path);
+            std::string component;            // tmp of one path
+            std::getline(ss, component, '/'); // getting the first path '/'
+            std::string tmp = "\0";
+            pathComponents.push_back("/" + component);
+            while (std::getline(ss, component, '/')) // loop for getting paths and stock it on the vector(pathComponents)
             {
-                // Etablish socket
-                sock = socket(domain,service, protocol);
-                if (sock == 0)
+                pathComponents.push_back(tmp + "/" + component);
+                tmp += "/" + component;
+            }
+            std::map<std::string, location>::iterator it;
+            for (size_t n = pathComponents.size(); n > 0; n--) // check if one of the pathComponents is exiting on the server(location)
+            {
+                it = Location.find(pathComponents[n]);
+                if (it != Location.end())
+                    return it;
+            }
+            return Location.end(); // if isn't exist we return end of map
+        }
+        bool methodChecker(std::string method, std::vector<std::string> Location)
+        {
+            for (size_t n = 0; n < Location.size(); n++)
+                if (Location[n] == method)
+                    return true;
+            return false;
+        }
+        void ft_accept(server servers)
+        {
+            int valread;
+            int addrlen = sizeof(address);
+            int errorFlag;
+            while (1)
+            {
+                new_socket = accept(sock, (struct sockaddr *)&address, (socklen_t *)&addrlen); // new FD that take the request
+                test_connection(new_socket);
+                char buffer[1024] = {0};
+                valread = read(new_socket, buffer, 30000); // reading the request from FD(new_socket) and stock it on buffer
+                write(new_socket, NULL, 1);
+                printf("------------------message sent-------------------\n");
+                std::cout << buffer << std::endl;
+                HttpRequest req = parse_http_request(buffer);                                                             // parsing the request
+                std::map<std::string, location>::iterator itLocation = locationChecker(req.path, servers.get_location()); // getting the location from the server
+                if (itLocation != servers.get_location().end())                                                           // check if the location is existing or not
                 {
-                    // perror("In socket...");
-                    exit(EXIT_FAILURE);
+                    if (methodChecker(req.method, itLocation->second.get_method())) // check if the method of request is existing on the location
+                    {
+                        // response
+                    }
+                    else
+                    {
+                        // error page
+                    }
                 }
-                // init adress struct
-                address.sin_family = domain;
-                address.sin_addr.s_addr = htonl(interface);
-                address.sin_port = htons(port);
-                memset(address.sin_zero, '\0', sizeof address.sin_zero);
-                // binding the connection 
-                connection = bind(sock, (struct sockaddr *)&address, sizeof(address));
-                test_connection(connection);
-                // listen
-                listening = listen(sock, backlog);
-                test_connection(listening);
-                ft_accept(servers);
             }
-            std::map<std::string, location>::iterator locationChecker(std::string path, std::map<std::string, location> &Location)
+        }
+        void test_connection(int itemToTest)
+        {
+            if (itemToTest < 0)
             {
-                std::vector<std::string> pathComponents; // vector of paths
-                std::stringstream ss(path);
-                std::string component; // tmp of one path
-                std::getline(ss, component, '/'); //getting the first path '/'
-                std::string tmp = "\0";
-                pathComponents.push_back("/" + component);
-                while (std::getline(ss, component, '/')) // loop for getting paths and stock it on the vector(pathComponents)
-                {
-                    pathComponents.push_back(tmp + "/" + component);
-                    tmp += "/" + component;
-                }
-                std::map<std::string, location>::iterator it;
-                for (size_t n = pathComponents.size(); n > 0; n--) // check if one of the pathComponents is exiting on the server(location)
-                {
-                    it = Location.find(pathComponents[n]);
-                    if (it != Location.end())
-                        return it;
-                }
-                return Location.end(); //if isn't exist we return end of map
+                perror("Connection Failed...");
+                exit(EXIT_FAILURE);
             }
-            bool methodChecker(std::string method,  std::vector<std::string> Location)
+        }
+        // Getter
+        struct sockaddr_in getAddress() { return address; }
+        int getSock() { return sock; }
+        int getConnection() { return connection; }
+        int getListening() { return listening; }
+        int getBacklog() { return backlog; }
+        // Setter
+        void setPort(int p) { this->port = p; }
+        ~Socket() {}
+    };
+    void connection_loop(std::vector<server> servers, Socket sockets[])
+    {
+        while (1)
+        {
+            fd_set readfds;
+            int valread;
+            int errorFlag;
+            FD_ZERO(&readfds);
+            for (int i = 0; i < servers.size(); i++)
+                FD_SET(sockets[i].getSock(), &readfds);
+            int max_fd = sockets[0].getSock();
+            for (int i = 0; i < servers.size(); i++)
             {
-                for(size_t n = 0; n < Location.size(); n++)
-                    if (Location[n] == method)
-                        return true;
-                return false;
+                if (sockets[i].getSock() > max_fd)
+                    max_fd = sockets[i].getSock();
             }
-            void ft_accept(server servers)
+            int activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+            if ((activity < 0) && (errno != EINTR))
+                printf("select error");
+            int new_socket;
+            for (int i = 0; i < servers.size(); i++)
             {
-                int valread;
-                int addrlen = sizeof(address);
-                int errorFlag;
-                while(1)
+                if (FD_ISSET(sockets[i].getSock(), &readfds))
                 {
-                    new_socket = accept(sock, (struct sockaddr *)&address, (socklen_t*)&addrlen); // new FD that take the request
-                    test_connection(new_socket);
-                    char buffer[30000] = {0};
-                    valread = read(new_socket , buffer, 30000); // reading the request from FD(new_socket) and stock it on buffer
-                    write(new_socket , NULL , 1);
+                    int addrlen = sizeof(sockets[i].getAddress());
+                    struct sockaddr_in address_tmp = sockets[i].getAddress();
+                    new_socket = accept(sockets[i].getSock(), (struct sockaddr *)&address_tmp, (socklen_t *)&addrlen); // new FD that take the request
+                    sockets[i].test_connection(new_socket);
+                    char buffer[1024] = {0};
+                    valread = read(new_socket, buffer, 30000); // reading the request from FD(new_socket) and stock it on buffer
+                    write(new_socket, NULL, 1);
                     printf("------------------message sent-------------------\n");
                     std::cout << buffer << std::endl;
-                    HttpRequest req = parse_http_request(buffer); // parsing the request
-                    std::map<std::string, location>::iterator itLocation = locationChecker(req.path, servers.get_location()); // getting the location from the server
-                    if (itLocation != servers.get_location().end()) // check if the location is existing or not
+                    HttpRequest req = parse_http_request(buffer);                                                                           // parsing the request
+                    std::map<std::string, location>::iterator itLocation = sockets[i].locationChecker(req.path, servers[i].get_location()); // getting the location from the server
+                    if (itLocation != servers[i].get_location().end())                                                                      // check if the location is existing or not
                     {
-                        if(methodChecker(req.method, itLocation->second.get_method())) // check if the method of request is existing on the location
+                        if (sockets[i].methodChecker(req.method, itLocation->second.get_method())) // check if the method of request is existing on the location
                         {
-                            //response
+                            // response
                         }
                         else
                         {
@@ -126,45 +194,20 @@ namespace ws{
                     }
                 }
             }
-            void test_connection(int itemToTest)
-            {
-                if (itemToTest < 0)
-                {
-                    // perror("Connection Failed...");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            // Getter
-            struct sockaddr_in getAddress() {return address;}
-            int getSock() {return sock;}
-            int getConnection() {return connection;}
-            int getListening() {return listening;}
-            int getBacklog() {return backlog;}
-            // Setter
-            void setPort(int p) {this->port = p;}
-            ~Socket(){}
-    };
+        }
+    }
     void socketStart(std::vector<server> servers)
     {
         std::cout << "Start..." << std::endl;
-    
+
         Socket sockets[servers.size()]; // initialize of sockets (multiple sockets) by how many server we have
 
         for (int i = 0; i < servers.size(); i++) // loop for create procces of each server
         {
-            pid_t pid = fork();
-            if (pid == -1) {
-                // error handling
-            } else if (pid == 0) {
-                sockets[i].setPort(atoi(servers[i].get_port().c_str())); // setting the port of the server
-                sockets[i].start(servers[i]); // starting the socket
-                exit(0);
-            }
+            sockets[i].setPort(atoi(servers[i].get_port().c_str())); // setting the port of the server
+            sockets[i].start(servers[i]);                            // starting the socket
         }
-        // wait for child processes to exit
-        for (int i = 0; i < 3; i++) {
-            wait(NULL);
-        }
+        connection_loop(servers, sockets);
     }
-}
 
+}
--- ts.cpp
+++ ts.cpp
@@ -25,3 +25,91 @@ int main() {
 
     return 0;
 }
+
+int main(int argc, char const *argv[]) {
+    int server_fd1, server_fd2, new_socket, max_sd, activity;
+    struct sockaddr_in address1, address2;
+    int addrlen = sizeof(address1);
+    char buffer[1024] = {0};
+    fd_set readfds;
+
+    // create server sockets
+    if ((server_fd1 = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((server_fd2 = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // bind sockets to ports
+    address1.sin_family = AF_INET;
+    address1.sin_addr.s_addr = INADDR_ANY;
+    address1.sin_port = htons(PORT1);
+    if (bind(server_fd1, (struct sockaddr *)&address1, sizeof(address1))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address2.sin_family = AF_INET;
+    address2.sin_addr.s_addr = INADDR_ANY;
+    address2.sin_port = htons(PORT2);
+    if (bind(server_fd2, (struct sockaddr *)&address2, sizeof(address2))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // listen for incoming connections
+    if (listen(server_fd1, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd2, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // clear the socket set
+        FD_ZERO(&readfds);
+
+        // add server sockets to set
+        FD_SET(server_fd1, &readfds);
+        FD_SET(server_fd2, &readfds);
+        max_sd = (server_fd1 > server_fd2) ? server_fd1 : server_fd2;
+
+        // add connected client sockets to set
+        // ...
+
+        // wait for activity on any socket
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        if ((activity < 0) && (errno!=EINTR)) {
+            printf("select error");
+        }
+
+        // handle activity on server socket 1
+        if (FD_ISSET(server_fd1, &readfds)) {
+            if ((new_socket = accept(server_fd1, (struct sockaddr *)&address1, (socklen_t*)&addrlen))<0) {
+                perror("accept failed");
+                exit(EXIT_FAILURE);
+            }
+
+            // handle new connection
+            // ...
+        }
+
+        // handle activity on server socket 2
+        if (FD_ISSET(server_fd2, &readfds)) {
+            if ((new_socket = accept(server_fd2, (struct sockaddr *)&address2, (socklen_t*)&addrlen))<0) {
+                perror("accept failed");
+                exit(EXIT_FAILURE);
+            }
+
+            // handle new connection
+            // ...
+        }
+
+        // handle activity on client sockets
