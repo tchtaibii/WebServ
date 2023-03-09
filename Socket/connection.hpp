5dfc3d151fc3c1bec6fe1296a165398dd0de6598
Taha Chtaibi <tchtaibi@e2r3p11.1337.ma>
2023-03-09 16:33:42 +0100

    multiplexer

--- Socket/connection.hpp
+++ Socket/connection.hpp
@@ -29,43 +29,59 @@ namespace ws
         }
         while (1)
         {
+            // Create a temporary fd_set to store the set of file descriptors
+            // that are ready to be read from.
             fd_set tmp_readfds = readfds;
+
+            // Wait for any of the file descriptors in the set to become
+            // readable using the select
             if (select(max + 1, &tmp_readfds, NULL, NULL, NULL) < 0)
                 throw std::runtime_error("Select error");
-
+            // Loop through all possible file descriptors, checking if each
+            // one is in the set of readable file descriptors.
             for (int fileD = 0; fileD <= max; fileD++)
             {
                 if (FD_ISSET(fileD, &tmp_readfds))
                 {
+                    // If the file descriptor is a listening socket, accept a new
+                    // connection and add the new socket to the set of readable
+                    // file descriptors.
                     if (std::count(fds.begin(), fds.end(), fileD))
                     {
                         new_socket = accept(fileD, NULL, NULL);
                         fcntl(new_socket, F_SETFL, O_NONBLOCK);
                         FD_SET(new_socket, &readfds);
-                        FD_SET(new_socket, &tmp_readfds);
                         clients.push_back(new_socket);
                         if (new_socket > max)
                             max = new_socket;
                     }
+                    // If the file descriptor is a client socket, read data from it.
                     else
                     {
                         char buffer[30001] = {0};
                         int valread = recv(fileD, buffer, 30000, 0);
                         if (valread <= 0)
                         {
-                            // close(fileD);
-                            // FD_CLR(fileD, &readfds);
-                            // FD_CLR(fileD, &tmp_readfds);
+                            // If the client socket has been closed or disconnected,
+                            // remove it from the set of readable file descriptors and
+                            // close the socket.
                             clients.erase(std::remove(clients.begin(), clients.end(), fileD), clients.end());
                             fds.erase(std::remove(fds.begin(), fds.end(), fileD), fds.end());
+                            close(fileD);
+                            FD_CLR(fileD, &readfds);
+                            max = *std::max_element(clients.begin(), clients.end());
                         }
                         else if (valread > 0)
                         {
+                            // If data has been read from the client socket, process
+                            // the request and send a response back to the client.
                             std::cout << buffer << std::endl;
                             send(fileD, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: 14\r\n\r\nHello, World!\r\n", 100, 0);
                         }
                         else
+                        {
                             throw std::runtime_error("Read error");
+                        }
                     }
                 }
             }
