99a8e287dd2aaa3a4c7fb0540fb30de09e7d9926
Taha Chtaibi <tchtaibi@e2r6p6.1337.ma>
2023-03-21 16:25:35 +0000

    fix some tkhrbi9

--- Session_management/Session.hpp
+++ Session_management/Session.hpp
@@ -2,18 +2,29 @@
 namespace ws
 {
     std::string generateSession()
-    {
-        std::string id_sesion = randomString(128);
-        std::cout << id_sesion << std::endl;
-        std::fstream file("./Session_management/sessionIds", std::ios::in | std::ios::out | std::ios::app);
+{
+    std::string id_sesion = ""; 
+    bool id_found = true;
+    while (id_found) {
+        id_sesion = std::to_string(time(NULL)) + "-" + std::to_string(rand()); // generate unique session ID
+        std::fstream file("./Session_management/sessionIds", std::ios::in);
         std::string line;
         while (std::getline(file, line))
         {
-            std::cout << line << std::endl;
+            size_t pos = line.find(id_sesion);
+            if (pos != std::string::npos) {
+                id_found = true;
+                break;
+            }
+            else {
+                id_found = false;
+            }
         }
-        size_t pos = line.find(id_sesion);
-        if (pos == std::string::npos)
-            file << "sessio_id=" << id_sesion << std::endl;
-        return id_sesion;
+        file.close(); // close file after reading from it
     }
+    std::fstream file("./Session_management/sessionIds", std::ios::out | std::ios::app);
+    file << "session_id=" << id_sesion << std::endl; // write session ID to file
+    file.close(); // close file after writing to it
+    return id_sesion;
+}
 }
